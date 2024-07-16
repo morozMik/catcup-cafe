86b76e7c6c0402ccd8af75a231291b455b25d278
morozMyk <mik.moroz0387@gmail.com>
2024-07-16 10:48:15 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'blue': '#1fb6ff',
+        'purple': '#7e5bef',
+        'pink': '#ff49db',
+        'orange': '#ff7849',
+        'green': '#78AD07',
+        'yellow': '#ffc82c',
+        'gray-dark': '#273444',
+        'gray': '#1E1E1E',
+        'gray-light': '#d3dce6',
+        'green-light': '#BEF34D'
+      },
+    },
+  },
+  plugins: [],
+}
+
